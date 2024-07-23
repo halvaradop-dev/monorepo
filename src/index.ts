59a1e669aea3482b816a8a82a37a324d34c0e011
halvaradop_ <hernanvid123@gmail.com>
2024-07-23 12:57:54 -0500

    chore: add code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-type ArgsFunction = (...args: any) => void
+import { ArgsFunction, Prettify } from "@halvaradop/ts-utility-types"
+
 
 export const executeAsync = (fun: ArgsFunction) => {
     // EXECUTE
 }
+
+export interface Store {
+    storage: any[],
+    index: number
+}
+
+export interface Config {
+    plugins: any[]
+}
+
+export type StoreConfig = Prettify<Store & Config>
+
+const config: StoreConfig = {
+    index: 0,
+    plugins: [],
+    storage: []
+}
+
+console.log(config)
