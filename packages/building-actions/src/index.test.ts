d16a94a8f52e21ba266d7c93fdb367de6be602ec
halvaradop <hernanvid123@gmail.com>
2024-08-21 23:11:32 -0500

    chore: setup workspaces on npm

---
+++ packages/building-actions/src/index.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "vitest"
+
+test("building-actions package", () => {
+    expect(0).toEqual(0)
+})
---
+++ packages/core/src/index.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "vitest"
+
+test("core package", () => {
+    expect(0).toEqual(0)
+})
