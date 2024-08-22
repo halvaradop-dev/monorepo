0b6704b30c7312fede7ac1785450f952547b597a
halvaradop <hernanvid123@gmail.com>
2024-08-22 14:40:16 -0500

    chore: setup package.json of every sub-module

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,7 +2,7 @@ import { buildingActionsAsync } from "@halvaradop/building-actions"
 
 export const coreAsync = async () => {
     console.log("core-package")
-    await buildingActionsAsync()
+    buildingActionsAsync()
 }
 
 coreAsync()
