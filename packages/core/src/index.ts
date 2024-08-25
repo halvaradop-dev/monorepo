4b2bad60aa121d36fd16b26902136ccb105bac67
halvaradop <hernanvid123@gmail.com>
2024-08-25 12:49:27 -0500

    chore: setup utils and core packages with entry points
    - Set up the `utils` and `chore` packatges within the project repo
    - Created and defined types, functions and properties in `utils`
      package, and their exports in `index.js`
    
    - Configured `utils/package.json` to properly set the export property
    - Defined two entry points in the package using export property
    	- One entry point includes all `.d.ts` files
    	- The other entry point, name `utils`, includes the js files
    - Release the initial version of `halvaradop/repo-utils`

---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+import { externalAsync, ReturnType } from "@halvaradop/repo-utils"
+
+type ExternalAsyncType = ReturnType<typeof externalAsync>
+
+console.log("ExternalAsyncRequest", "ExternalType", "FixExternalFunction")
