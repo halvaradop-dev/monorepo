c23bd788a089c8255e52a711eefe0cc52a0eafa4
halvaradop <hernanvid123@gmail.com>
2024-09-15 08:30:39 -0500

    chore: install turborepo

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -6,3 +6,8 @@ export const externalAsync = async (external: (...args: any) => void) => {
     // fetching data
     external()
 }
+
+export const config: Record<string, string> = {
+    path: "./src",
+    name: "utils",
+}
