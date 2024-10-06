29dc7665c744d1b609a28329e70214016e17a890
Hernan Alvarado <hernanvid123@gmail.com>
2024-10-06 11:07:32 -0500

    chore: add tests in `core`, `css` and `jsx` packages

---
+++ packages/core/test/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, test, expect } from "vitest"
+
+describe("core package tests", () => {
+    test("Check if two numbers are equals", () => {
+        expect(1 + 1).toBe(2)
+    })
+})
